abc137dcf4b1bef916d14e8701ec9e2b9f149efe
Ibnu Abbas Al Harun <walawalaskuyy@gmail.com>
2026-03-24 19:13:26 +0700

    mengubah lg lg dah

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,3 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyB__2qJtb9nGRGpxOk8QLXTwCBSBRaxv_I",
-  authDomain: "taskleveling-198f3.firebaseapp.com",
-  projectId: "taskleveling-198f3",
-  storageBucket: "taskleveling-198f3.firebasestorage.app",
-  messagingSenderId: "535024788397",
-  appId: "1:535024788397:web:0e62022427eb390751b724"
+ 
 };
