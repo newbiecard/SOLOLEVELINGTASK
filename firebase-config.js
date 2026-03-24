413e2a986613b2534f579d7d0525408e61cd955a
Ibnu Abbas Al Harun <walawalaskuyy@gmail.com>
2026-03-24 19:04:10 +0700

    mengubah lg dah

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB__2qJtb9nGRGpxOk8QLXTwCBSBRaxv_I",
   authDomain: "taskleveling-198f3.firebaseapp.com",
@@ -12,6 +6,3 @@ const firebaseConfig = {
   messagingSenderId: "535024788397",
   appId: "1:535024788397:web:0e62022427eb390751b724"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
